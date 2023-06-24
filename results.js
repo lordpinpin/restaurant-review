db26d72d6c5defb726ae3ccf9380bc25d9b11dd4
lordpinpin <lord_pinpin@dlsu.edu.ph>
2023-06-24 18:28:11 +0800

    90% restaurant results

--- results.js
+++ results.js
@@ -4,6 +4,9 @@ const dropdownReviews = document.querySelectorAll('.sample-review');
 const sortby = document.querySelector('.sortby');
 const sortbyNav = document.querySelector('.sortby-nav');
 const sortbyIcon = document.querySelector('.sortby-icon');
+const sortbyItem = document.querySelectorAll('.sortby-item');
+const sortbyName = document.querySelector('.sortby-name');
+
 
 sortby.addEventListener('click', ()=> {
     sortby.classList.toggle('revealed');
@@ -11,7 +14,14 @@ sortby.addEventListener('click', ()=> {
     sortbyIcon.classList.toggle('active');
 });
 
-
+for (let i = 0; i < sortbyItem.length; i++) {
+    sortbyItem[i].addEventListener("click", function() {
+        sortbyName.innerText = ("Sort by: " + sortbyItem[i].innerText);
+        sortby.classList.remove('revealed');
+        sortbyNav.classList.remove('revealed');
+        sortbyIcon.classList.remove('active');
+    });
+}
 
 for (let i = 0; i < dropdown.length; i++) {
     dropdown[i].addEventListener("click", function() {
